6f256c5c933d2a5e2c79d9b05f07137f218fb91f
lanhun <445052471@qq.com>
2016-07-11 21:57:45 +0800

    移除目标类中的测试方法

--- src/main/java/com/lanhun/distributedSequence/CodeGeneratorImpl.java
+++ src/main/java/com/lanhun/distributedSequence/CodeGeneratorImpl.java
@@ -1,11 +1,9 @@
 package com.lanhun.distributedSequence;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import redis.clients.util.Pool;
 
 public class CodeGeneratorImpl implements CodeGenerator {
@@ -15,28 +13,9 @@ public class CodeGeneratorImpl implements CodeGenerator {
 	public void setJedisPool(Pool<Jedis> jedisPool) {
 		this.jedisPool = jedisPool;
 	}
+
 	public String generate(String prefix) {
 		return generate(prefix, "yyyyMMdd");
-	}
-
-	public static void main(String[] args) {
-		String host = "127.0.0.1";
-		int port = 6379;
-
-		List<String> codes = new ArrayList<String>();
-		JedisPool jedisPool = new JedisPool(host, port);
-		CodeGenerator generator = new CodeGeneratorImpl();
-		((CodeGeneratorImpl) generator).setJedisPool(jedisPool);
-		long t = System.currentTimeMillis();
-		String code = null;
-		for (int i = 0; i < 1; i++) {
-			code = generator.generate("O2O");
-			codes.add(code);
-			System.out.println(code);
-		}
-		t = System.currentTimeMillis() - t;
-		System.out.println("take" + t + " ms");
-		System.out.println("total " + codes.size() + " codes");
 	}
 
 	public static String format(Long i, int len) {
@@ -51,8 +30,8 @@ public class CodeGeneratorImpl implements CodeGenerator {
 	public String generate(String prefix, String format, int len) {
 		String storeType = "seq:" + prefix;
 		Jedis jedis = jedisPool.getResource();
-		if(jedis.setnx(storeType, 0+"")>0){
-			jedis.expire(storeType, 3600*24);
+		if (jedis.setnx(storeType, 0 + "") > 0) {
+			jedis.expire(storeType, 3600 * 24);
 		}
 		Long i = jedis.incr(storeType);
 		jedis.close();
@@ -60,7 +39,8 @@ public class CodeGeneratorImpl implements CodeGenerator {
 		long t = Long.parseLong(time.get(0) + "000");
 		long m = Long.parseLong(time.get(1));
 		t = t + m / 1000;
-		return String.format("%s%s%0" + len + "d", prefix, DateUtils.format(format, new Date(t)), i);
+		return String.format("%s%s%0" + len + "d", prefix,
+				DateUtils.format(format, new Date(t)), i);
 	}
 
 	public String generate(String prefix, String format) {
